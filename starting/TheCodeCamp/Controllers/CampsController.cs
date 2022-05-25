fc57127808accbe271e7c9b2a17d5b376419f47d
vidyakore <vidyakore3108@gmail.com>
2022-05-25 16:46:57 +0530

    checked status code

---
+++ starting/TheCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace TheCodeCamp.Controllers
+{
+    public class CampsController : ApiController
+    {
+        // GET: Camps
+        public IHttpActionResult Get()  //IHttpActionResult allows us to return both payloads and status codes
+        {
+            //return BadRequest("It's Okay");
+            return Ok(new { Name = "Shawn", Occupation = "Teacher" });
+        }
+    }
+}
